daab7d6e52cc597c8b4813864868d28d10c972f7
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2022-07-11 14:09:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from pynput.mouse import Button, Controller
 import time
-import keyboard
+from pynput import keyboard
 mouse = Controller()
-i = 1
+
+def on_press(key):
+    try:
+        print('{0}'.format(
+            key.char))
+    except AttributeError:
+        print('{0}'.format(
+            key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
 
 
-while i < 6:  # making a loopa
-    print("working")
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('a'):  # if key 'q' is pressed
-            print('You Pressed A Key!')
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop will break
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
