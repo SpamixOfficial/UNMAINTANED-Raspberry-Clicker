fc8fb957aec4114e7dfe9c8e56ff06e31c82585c
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2022-07-11 13:13:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from pynput.mouse import Button, Controller
 import time
 import keyboard
 mouse = Controller()
+i = 1
 
-while True:  # making a loopa
+
+while i < 6:  # making a loopa
+    print("working")
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed
+        if keyboard.is_pressed('a'):  # if key 'q' is pressed
             print('You Pressed A Key!')
             break  # finishing the loop
     except:
